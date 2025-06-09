e731bc620e8bfb1dcba31d6a8861db8d5f0ded67
chrinore <chrinoren@gmail.com>
2025-06-10 00:24:33 +0100

    nothing work in the front side e

--- sqlfront/src/App.jsx
+++ sqlfront/src/App.jsx
@@ -1,15 +1,15 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Login from "./components/Log"
-import Register from "./components/Register"
-import Todolist from "./components/TodoList"
+import Login from "./components/Log";
+import Register from "./components/Register";
+import Todolist from "./components/TodoList";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login/>}/>
-      <Route path= "/register" element={<Register/>}/>
-      <Route path="/todolist" element={<Todolist/>}/>
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/todolista" element={<Todolist />} />
     </Routes>
   );
 }
