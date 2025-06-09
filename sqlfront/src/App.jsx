7a934d7d54a9908f96ace4591d5640ccd998cd7b
chrinore <chrinoren@gmail.com>
2025-06-10 00:25:20 +0100

    nothing work in the front side e

--- sqlfront/src/App.jsx
+++ sqlfront/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/todolista" element={<Todolist />} />
+      <Route path="/todolist" element={<Todolist />} />
     </Routes>
   );
 }
