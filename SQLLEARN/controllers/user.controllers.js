81cf39e402f58dd5e77acf8ff03e5d0ef89a5c01
chrinore <chrinoren@gmail.com>
2025-06-11 21:58:38 +0100

    suppression de la a branch secondaire et Actualisation

--- SQLLEARN/controllers/user.controllers.js
+++ SQLLEARN/controllers/user.controllers.js
@@ -152,7 +152,6 @@ exports.connexion = async (req, res) => {
 
     if (!userfinder) {
       return res.status(401).json({ message: "User not found" });
-      // ohooh
     }
 
     const compare = await argon2.verify(userfinder.password, password);
