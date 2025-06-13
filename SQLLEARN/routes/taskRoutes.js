ddab0fc598545dc715e9658d0197709933d8dec4
chrinore <chrinoren@gmail.com>
2025-06-13 19:03:06 +0100

    Projet fini fonctionel

--- SQLLEARN/routes/taskRoutes.js
+++ SQLLEARN/routes/taskRoutes.js
@@ -5,14 +5,8 @@ const prisma = require("../config/prismaClient");
 const router = express.Router();
 const { limiter } = require("../middleware/middleware");
 
-const { addtask, addtask8 } = require("../controllers/addTask.controllers");
+const { addtask } = require("../controllers/addTask.controllers");
 
 router.post("/api/addtask", addtask);
-
-router.post("/api/88", async (req, res) => {
-  const { task } = req.body;
-  const user_id = req.session.user_id;
-  await addtask8(task, user_id);
-});
 
 module.exports = router;
