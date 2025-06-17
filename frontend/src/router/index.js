c483957f471fee67f16babd4ee678e41f6e49815
chrinore <chrinoren@gmail.com>
2025-06-18 00:15:58 +0100

    add block navigation by link

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,8 @@ import { createWebHistory } from "vue-router";
 import Login from "../pages/Login.vue";
 import Register from "../pages/Register.vue";
 import Todopage from "../pages/Todopage.vue";
+import { useUserStore } from "../store/UserTask.service";
+
 const routes = [
   { path: "/", component: Login },
   { path: "/register", component: Register },
@@ -12,6 +14,17 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+// --
+router.beforeEach((to, from, next) => {
+  const userStore = useUserStore();
+
+  if (to.meta.requiresAuth && !userStore.user) {
+    next("/");
+  } else {
+    next();
+  }
 });
 
 export default router;
