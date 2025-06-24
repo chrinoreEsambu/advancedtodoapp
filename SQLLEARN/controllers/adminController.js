e3fc6beb6c23309578303ade71781c37877f412d
chrinore <chrinoren@gmail.com>
2025-06-24 23:59:36 +0100

    ajout du controller admin

--- SQLLEARN/controllers/adminController.js
+++ SQLLEARN/controllers/adminController.js
@@ -41,7 +41,7 @@ exports.adminConnexion = async (req, res) => {
     const { mail, password } = req.body;
 
     const userfinder = await prisma.users.findUnique({
-      where: { data: { mail: mail } },
+      where: { mail },
     });
     if (!userfinder) {
       return res.status(401).json({ message: "User not found" });
