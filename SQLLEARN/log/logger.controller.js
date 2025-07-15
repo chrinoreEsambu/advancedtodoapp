553278ad2de31db4c2924414d9b075360a2452b4
chrinore <chrinoren@gmail.com>
2025-07-15 17:49:13 +0100

    ajout logger feature

--- SQLLEARN/log/logger.controller.js
+++ SQLLEARN/log/logger.controller.js
@@ -2,12 +2,17 @@ const prisma = require("../config/prismaClient");
 const session = require("express-session");
 
 const logAdminAction = async (adminId, action, details) => {
-    try {
-       f (!adminId) {
+  try {
+    if (!adminId) {
       console.warn("Admin non identifié. Impossible de logger l'action.");
       return;
     }
-    } catch (error) {
-        
-    }
+    await prisma.logs.create({
+      data: {
+        adminId,
+        action,
+        details,
+      },
+    });
+  } catch (error) {}
 };
