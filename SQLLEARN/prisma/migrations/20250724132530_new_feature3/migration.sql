6d87f97d61c6107dc45dcafe292c815fda6e8a35
chrinore <chrinoren@gmail.com>
2025-07-24 14:31:49 +0100

    changement de model db

---
+++ SQLLEARN/prisma/migrations/20250724132530_new_feature3/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - The values [accepted] on the enum `Tasks_taskState` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterTable
+ALTER TABLE `tasks` MODIFY `taskState` ENUM('todo', 'inprogress', 'request', 'accept', 'done') NULL DEFAULT 'todo';
