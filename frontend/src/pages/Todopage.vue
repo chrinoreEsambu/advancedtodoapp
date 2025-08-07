9e00349a3b7b7483ff56510c1c370d70cc446226
chrinore <chrinoren@gmail.com>
2025-08-07 23:58:46 +0100

    style by claude

--- frontend/src/pages/Todopage.vue
+++ frontend/src/pages/Todopage.vue
@@ -181,6 +181,8 @@
               >
                 <div class="task-info">
                   {{ task.task }}
+                  <br />
+                  <lavel class="description">{{ task.description }}</lavel>
                 </div>
                 <div class="task-actions">
                   <button
