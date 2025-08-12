91e302d7f9af2e42f5945ab99a3bf083b183cbe4
chrinore <chrinoren@gmail.com>
2025-08-12 22:55:28 +0100

    ready to go prod06

--- SQLLEARN/middleware/middleware.js
+++ SQLLEARN/middleware/middleware.js
@@ -8,12 +8,12 @@ const prisma = require("../config/prismaClient");
 const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
 
 exports.usersession = session({
+  name: "sessionId", // Nom personnalisé pour le cookie
   cookie: {
-    maxAge: 7 * 24 * 60 * 60 * 1000,
-    // sameSite: "none",
-    // secure: true,
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 jours
+    httpOnly: true, // Garde au moins cette sécurité
   },
-  secret: process.env.SESSION_SECRET || "default_secret",
+  secret: process.env.SESSION_SECRET || "your_fallback_secret_here",
   resave: false,
   saveUninitialized: false,
   store: new PrismaSessionStore(prisma, {
