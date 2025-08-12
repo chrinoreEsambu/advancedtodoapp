3721f2762f2d4723574adb2649a02f56ae438120
chrinore <chrinoren@gmail.com>
2025-08-12 14:17:41 +0100

    ready to go prod01

--- frontend/src/store/UserTask.service.js
+++ frontend/src/store/UserTask.service.js
@@ -27,7 +27,7 @@ export const useUserStore = defineStore("user", {
 
   actions: {
     async login(user_id, password) {
-      const res = await api.post("/connexion", { user_id, password });
+      const res = await api.post("/api/connexion", { user_id, password });
       this.user = res.data.user;
       this.isAuthenticated = true;
       localStorage.setItem("user", JSON.stringify(res.data.user));
